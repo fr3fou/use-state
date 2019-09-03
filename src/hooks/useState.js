c84e809969f4d2f0be5d12238c4bbe58dad89db9
Simo Aleksandrov <simo3003@me.com>
2019-09-03 19:38:22 +0300

    improv: simplify logic

--- src/hooks/useState.js
+++ src/hooks/useState.js
@@ -1,21 +1,7 @@
 import React from 'react'
 
-const SET_VALUE = 'SET_VALUE'
-
-const actions = {
-  [SET_VALUE]: (_, newVal) => {
-    return newVal
-  },
-}
-
-function reducer(state, { type, payload }) {
-  const handler = actions[type]
-
-  if (handler) {
-    return handler(state, payload)
-  }
-
-  return state
+function reducer(_, value) {
+  return value
 }
 
 function useState(initalValue) {
@@ -25,11 +11,11 @@ function useState(initalValue) {
 
   const setValue = v => {
     if (typeof v === 'function') {
-      dispatch({ type: SET_VALUE, payload: v(value) })
+      dispatch(v(value))
       return
     }
 
-    dispatch({ type: SET_VALUE, payload: v })
+    dispatch(v)
   }
 
   return [value, setValue]
