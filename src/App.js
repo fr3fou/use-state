5b4ed648fb9382b63e2729b27da78facb9b99ba9
Simo Aleksandrov <simo3003@me.com>
2019-09-03 17:01:16 +0300

    feat: not working yet

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import logo from './logo.svg'
+import useState from './hooks/useState'
 import './App.css'
 
 function App() {
-  // noop
-  const counter = 1
-  // noop
-  function setCounter() {}
+  const [counter, setCounter] = useState(0)
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>useState()</h1>
-
-        <button className="App-button" onClick={() => setCounter(a => a + 1)}>
+        <button
+          className="App-button"
+          onClick={() => {
+            setCounter(a => a + 1)
+          }}
+        >
           Increment counter!
         </button>
         <p>{counter}</p>
--- src/hooks/useState.js
+++ src/hooks/useState.js
@@ -1,1 +1,39 @@
 import React from 'react'
+
+const SET_VALUE = 'SET_VALUE'
+
+const actions = {
+  [SET_VALUE]: (_, newVal) => {
+    return newVal
+  },
+}
+
+function reducer(state, { type, payload }) {
+  const handler = actions[type]
+
+  if (handler) {
+    handler(state, payload)
+  }
+
+  return state
+}
+
+function useState(initalValue) {
+  const [value, dispatch] = React.useReducer(reducer, initalValue)
+
+  const setValue = v => {
+    if (typeof v === 'function') {
+      console.log('here')
+      dispatch({ type: SET_VALUE, payload: v(value) })
+      return
+    }
+
+    dispatch({ type: SET_VALUE, payload: v })
+  }
+
+  console.log(value)
+
+  return [value, setValue]
+}
+
+export default useState
