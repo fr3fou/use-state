6b4932f570208b29c72594127c2926fe49cdce57
Simo Aleksandrov <simo3003@me.com>
2019-09-03 16:49:56 +0300

    feat: basic css

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
+  // noop
+  const counter = 1
+  // noop
+  function setCounter() {}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>useState()</h1>
+
+        <button className="App-button" onClick={() => setCounter(a => a + 1)}>
+          Increment counter!
+        </button>
+        <p>{counter}</p>
       </header>
     </div>
   )
